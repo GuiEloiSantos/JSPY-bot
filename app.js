21c5b38ae99eaa5a6bd6f641ddef05772d932d5b
GuiEloi <gcunhaeloi@gmail.com>
2017-09-24 18:27:16 -0300

    First commit

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let config = require('config');
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-    accessToken: config.get('ACCES_TOKEN'),
-    verifyToken: config.get('VERIFY_TOKEN'),
-    appSecret: config.get('APP_SECRET')
+    accessToken: 'EAAWXRVTfdHwBAFJKrnHQ313fFHKRzrcF0BUieSL8MholU2EykboibHbDNXglThXVv2X1gZB2O1Sc7wdm5dcMtBjdGKoVFYU0YyCIcly0oDkyYfRfN4NkACqZBwmmczor3mnXcsmLbZASm9oBfpkL3idSSyVnwZCWg1HsXpk72gZDZD',
+    verifyToken: '784df74df75bda0307b6c878380e6359',
+    appSecret: '8397aa990919058966cf1049b13957c5'
 });
 bot.on('message', (payload, chat) => {
     const text = payload.message.text;
