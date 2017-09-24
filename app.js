63d7095daee298175a9e7153794453a2ee9b7ae0
GuiEloi <gcunhaeloi@gmail.com>
2017-09-24 17:09:48 -0300

    First commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let index = require('./routes/index');
+let users = require('./routes/users');
 
-var app = express();
+let app = express();
 //test
 
 // view engine setup
@@ -26,12 +26,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === "784df74df75bda0307b6c878380e6359") {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+const BootBot = require('bootbot');
+
+const bot = new BootBot({
+    accessToken:  process.env.ACCES_TOKEN,
+    verifyToken:  process.env.VERIFY_TOKEN,
+    appSecret:  process.env.APP_SECRET
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
+
+
+bot.on('message', (payload, chat) => {
+    const text = payload.message.text;
+chat.say(`Echo: ${text}`);
+});
+
 
 // error handler
 app.use(function(err, req, res, next) {
