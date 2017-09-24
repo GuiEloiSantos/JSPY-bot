662d8b9fb4eeca3eb2903ee8f99c8c881ba2c26a
GuiEloi <gcunhaeloi@gmail.com>
2017-09-24 18:00:31 -0300

    First commit

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 let express = require('express');
 let app = express();
-app.set('env', process.env.ENV);
+let config = require('config');
+
 
 
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-    accessToken:  process.env.ACCES_TOKEN,
-    verifyToken:  process.env.VERIFY_TOKEN,
-    appSecret:  process.env.APP_SECRET
+    accessToken: config.get('ACCES_TOKEN'),
+    verifyToken: config.get('VERIFY_TOKEN'),
+    appSecret: config.get('APP_SECRET')
 });
 bot.on('message', (payload, chat) => {
     const text = payload.message.text;
-chat.say(`Echo: ${text}`);
+    chat.say(`Echo: ${text}`);
 });
 
 bot.start();
