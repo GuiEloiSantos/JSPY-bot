02227c54a4eb94dc3fc54e8c1af9f016b21f2bcf
GuiEloi <gcunhaeloi@gmail.com>
2017-09-25 00:20:14 -0300

    First commit

--- app.js
+++ app.js
@@ -5,9 +5,77 @@ const bot = new BootBot({
     verifyToken: process.env.VERIFY_TOKEN,
     appSecret: process.env.APP_SECRET
 });
+
+bot.setGreetingText("Olá, precisa de mais informações sobre pratos e o Mon Del? Tenho todo o Menu com fotos e detalhes, assim como promoções exclusivas, quer dar uma olhada?");
+bot.setGetStartedButton("Eu adoraria!");
+bot.setPersistentMenu([
+    {
+        title: 'Ver Pratos',
+        type: 'nested',
+        call_to_actions: [
+            {
+                title: 'Entradas',
+                type: 'postback',
+                payload: 'FIRST'
+            },
+            {
+                title: 'Prato Principal',
+                type: 'postback',
+                payload: 'MAIN'
+            },
+            {
+                title: 'Sobremesa',
+                type: 'postback',
+                payload: 'DESERT'
+            }
+        ]
+    },
+    {
+        title: 'Bebidas',
+        type: 'nested',
+        call_to_actions: [
+            {
+                title: 'Sem Álcool',
+                type: 'postback',
+                payload: 'N_ALC'
+            },
+            {
+                title: 'Alcoólicas',
+                type: 'postback',
+                payload: 'ALC'
+            },
+            {
+                title: 'Vinhos',
+                type: 'postback',
+                payload: 'WINE'
+            }
+        ]
+    },
+    {
+        title: 'Outros',
+        type: 'nested',
+        call_to_actions: [
+            {
+                title: 'Fazer reserva',
+                type: 'postback',
+                payload: 'BOOKING'
+            },
+            {
+                title: 'Deixar Feedback',
+                type: 'postback',
+                payload: 'FEEDBACK'
+            },
+            {
+                title: 'Promoções',
+                type: 'postback',
+                payload: 'Promoções'
+            }
+        ]
+    }
+], true);
 bot.on('message', (payload, chat) => {
     const text = payload.message.text;
     chat.say(`Echo: ${text}`);
 });
 console.log(process.env);
-bot.start(process.env.PORT||80);
+bot.start(process.env.PORT || 80);
