b5b630ffb778b1a6f40fcbbe92e4b2028d1bf63b
GuiEloi <gcunhaeloi@gmail.com>
2017-09-25 02:35:26 -0300

    First commit

--- app.js
+++ app.js
@@ -80,15 +80,15 @@ bot.on('message', (payload, chat) => {
                 {
                     title: "Classic T-Shirt Collection",
                     subtitle: "See all our colors",
-                    image_url: "http://peterssendreceiveapp.ngrok.io/img/collection.png",
+                    image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
                     buttons: [
                         {
                             title: "View",
                             type: "web_url",
-                            url: "http://peterssendreceiveapp.ngrok.io/collection",
+                            url: "https://peterssendreceiveapp.ngrok.io/collection",
                             messenger_extensions: true,
                             webview_height_ratio: "tall",
-                            fallback_url: "http://calm-castle-79707.herokuapp.com/"
+                            fallback_url: "https://calm-castle-79707.herokuapp.com/"
                         }
                     ]
                 },
@@ -97,22 +97,22 @@ bot.on('message', (payload, chat) => {
                     subtitle: "See all our colors",
                     default_action: {
                         type: "web_url",
-                        url: "http://peterssendreceiveapp.ngrok.io/view?item=100",
+                        url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
                         messenger_extensions: true,
                         webview_height_ratio: "tall",
-                        fallback_url: "http://calm-castle-79707.herokuapp.com/"
+                        fallback_url: "https://calm-castle-79707.herokuapp.com/"
                     }
                 },
                 {
                     title: "Classic Blue T-Shirt",
-                    image_url: "http://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                    image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
                     subtitle: "100% Cotton, 200% Comfortable",
                     default_action: {
                         type: "web_url",
-                        url: "http://peterssendreceiveapp.ngrok.io/view?item=101",
+                        url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
                         messenger_extensions: true,
                         webview_height_ratio: "tall",
-                        fallback_url: "http://calm-castle-79707.herokuapp.com/"
+                        fallback_url: "https://calm-castle-79707.herokuapp.com/"
                     },
 
                 }
@@ -120,13 +120,14 @@ bot.on('message', (payload, chat) => {
             [{
                 title: "Shop Now",
                 type: "web_url",
-                url: "http://peterssendreceiveapp.ngrok.io/shop?item=101",
+                url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
                 messenger_extensions: true,
                 webview_height_ratio: "tall",
-                fallback_url: "http://calm-castle-79707.herokuapp.com/"
+                fallback_url: "https://calm-castle-79707.herokuapp.com/"
             }]
-        ).then((result) => {
+        ).then((result, err) => {
                 console.log(result);
+                console.log(err);
             }
         );
     });
