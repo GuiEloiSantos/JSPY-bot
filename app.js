5d7df804044931b522cbbb2f7be7535aed08c13b
GuiEloi <gcunhaeloi@gmail.com>
2017-09-25 01:59:06 -0300

    First commit

--- app.js
+++ app.js
@@ -80,58 +80,57 @@ bot.on('message', (payload, chat) => {
 });
 bot.on('postback:START', (payload, chat) => {
         chat.getUserProfile().then((user) => {
-            chat.say(`Oi, ${user.first_name}. Será um prazer atender você! Aqui está as opções em que posso te ajudar:`).then(
-                chat.sendListTemplate([
-                        {
-                            title: "Classic T-Shirt Collection",
-                            subtitle: "See all our colors",
-                            image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                            buttons: [
-                                {
-                                    title: "View",
-                                    type: "web_url",
-                                    url: "https://peterssendreceiveapp.ngrok.io/collection",
-                                    messenger_extensions: true,
-                                    webview_height_ratio: "tall",
-                                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                                }
-                            ]
-                        },
-                        {
-                            title: "Classic White T-Shirt",
-                            subtitle: "See all our colors",
-                            default_action: {
+            chat.sendListTemplate([
+                    {
+                        title: "Classic T-Shirt Collection",
+                        subtitle: "See all our colors",
+                        image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                        buttons: [
+                            {
+                                title: "View",
                                 type: "web_url",
-                                url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                url: "https://peterssendreceiveapp.ngrok.io/collection",
                                 messenger_extensions: true,
                                 webview_height_ratio: "tall",
                                 fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                             }
-                        },
-                        {
-                            title: "Classic Blue T-Shirt",
-                            image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                            subtitle: "100% Cotton, 200% Comfortable",
-                            default_action: {
-                                type: "web_url",
-                                url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                                messenger_extensions: true,
-                                webview_height_ratio: "tall",
-                                fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                            },
-
+                        ]
+                    },
+                    {
+                        title: "Classic White T-Shirt",
+                        subtitle: "See all our colors",
+                        default_action: {
+                            type: "web_url",
+                            url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                            messenger_extensions: true,
+                            webview_height_ratio: "tall",
+                            fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                         }
-                    ],
-                    [{
-                        title: "Shop Now",
-                        type: "web_url",
-                        url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                        messenger_extensions: true,
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                    }]
-                )
+                    },
+                    {
+                        title: "Classic Blue T-Shirt",
+                        image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                        subtitle: "100% Cotton, 200% Comfortable",
+                        default_action: {
+                            type: "web_url",
+                            url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                            messenger_extensions: true,
+                            webview_height_ratio: "tall",
+                            fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                        },
+
+                    }
+                ],
+                [{
+                    title: "Shop Now",
+                    type: "web_url",
+                    url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                    messenger_extensions: true,
+                    webview_height_ratio: "tall",
+                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                }]
             );
+            chat.say(`Oi, ${user.first_name}. Será um prazer atender você! Aqui está as opções em que posso te ajudar:`);
         });
     }
 );
