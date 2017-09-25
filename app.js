3ba9dcea554681546483357c8fe741e67e00c28b
GuiEloi <gcunhaeloi@gmail.com>
2017-09-25 02:12:36 -0300

    First commit

--- app.js
+++ app.js
@@ -74,12 +74,66 @@ bot.setPersistentMenu([
     }
 ]);
 bot.on('message', (payload, chat) => {
-    const text = payload.message.text;
-    console.log(payload);
-    chat.say(`Echo: ${text}`);
+    chat.say(`Oi, ${user.first_name}. Será um prazer atender você! Aqui está as opções em que posso te ajudar:`);
+    chat.sendListTemplate([
+            {
+                title: "Classic T-Shirt Collection",
+                subtitle: "See all our colors",
+                image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                buttons: [
+                    {
+                        title: "View",
+                        type: "web_url",
+                        url: "https://peterssendreceiveapp.ngrok.io/collection",
+                        messenger_extensions: true,
+                        webview_height_ratio: "tall",
+                        fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                ]
+            },
+            {
+                title: "Classic White T-Shirt",
+                subtitle: "See all our colors",
+                default_action: {
+                    type: "web_url",
+                    url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    messenger_extensions: true,
+                    webview_height_ratio: "tall",
+                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                }
+            },
+            {
+                title: "Classic Blue T-Shirt",
+                image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                subtitle: "100% Cotton, 200% Comfortable",
+                default_action: {
+                    type: "web_url",
+                    url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                    messenger_extensions: true,
+                    webview_height_ratio: "tall",
+                    fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                },
+
+            }
+        ],
+        [{
+            title: "Shop Now",
+            type: "web_url",
+            url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+            messenger_extensions: true,
+            webview_height_ratio: "tall",
+            fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+        }]
+    ).then((result) =>
+        {
+            console.log(result);
+        }
+    );
+
 });
 bot.on('postback:START', (payload, chat) => {
         chat.getUserProfile().then((user) => {
+            chat.say(`Oi, ${user.first_name}. Será um prazer atender você! Aqui está as opções em que posso te ajudar:`);
             chat.sendListTemplate([
                     {
                         title: "Classic T-Shirt Collection",
@@ -135,7 +189,7 @@ bot.on('postback:START', (payload, chat) => {
                 console.log(err);
             }
             );
-            //chat.say(`Oi, ${user.first_name}. Será um prazer atender você! Aqui está as opções em que posso te ajudar:`);
+
         });
     }
 );
